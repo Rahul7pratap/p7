047cd3016f9c24946af725b7226881a2038dd5f8
Rahul Pratap Singh <Rahul7pratap@gmail.com>
2022-05-06 17:36:07 +0000

    first commit

---
+++ test-artifact-id2/src/main/java/com/demo2/test_artifact_id2/App.java
@@ -0,0 +1,13 @@
+package com.demo2.test_artifact_id2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Radhe Radhe!!!!!" );
+    }
+}
---
+++ test-artifact-id2/src/test/java/com/demo2/test_artifact_id2/AppTest.java
@@ -0,0 +1,20 @@
+package com.demo2.test_artifact_id2;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
